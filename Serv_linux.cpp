46d50b7dd1c5e1fa63aef90b0b4695709ae5a453
andreyka <andreyka@andreyka.ru>
2022-08-25 14:43:25 +0300

    Server formatting

--- Serv_linux.cpp
+++ Serv_linux.cpp
@@ -1,5 +1,6 @@
 /* A simple server in the internet domain using TCP */
-#include <stdio.h>
+#include <iostream>
+//#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -15,35 +16,38 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd;
+     int sockfd, newsockfd, n;
      int portno = 21947;
      socklen_t clilen;
      char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
-     int n;
+     
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
+     if (sockfd < 0) { error("ERROR opening socket"); }
+     
      bzero((char *) &serv_addr, sizeof(serv_addr));
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
+     		 sizeof(serv_addr)) < 0) 
+     		 { error("ERROR on binding"); }
+              
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
      newsockfd = accept(sockfd, 
                  (struct sockaddr *) &cli_addr, 
                  &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
+     if (newsockfd < 0) { error("ERROR on accept"); }
+     
      bzero(buffer,256);
      n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
+     if (n < 0) { error("ERROR reading from socket"); }
+     
+     std::cout << "Here is the message: " << buffer << '\n';
      n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
+     if (n < 0) { error("ERROR writing to socket"); }
+     
      close(newsockfd);
      close(sockfd);
      return 0; 
