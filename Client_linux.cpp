e5a5aff84ef452e65f174607c6a7a752a18ab490
andreyka <andreyka@andreyka.ru>
2022-08-26 16:50:21 +0300

    No errors

--- Client_linux.cpp
+++ Client_linux.cpp
@@ -70,6 +70,5 @@ int main(int argc, char *argv[])
 	}
     
     close(sockfd);
-    main(argc, argv);
     return 0;
 }
--- Serv_linux.cpp
+++ Serv_linux.cpp
@@ -6,17 +6,11 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void error(const char *msg) {
-	std::cout << msg;
-	exit(1);
-}
-
 int try_accept(int sockfd,	struct sockaddr_in cli_addr, socklen_t clilen) {
 	std::cout << "Waiting for acccept\n";
 	int newsockfd = accept(sockfd, 
 			(struct sockaddr *) &cli_addr, 
 			&clilen);
-	if (newsockfd < 0) { error("ERROR on accept"); }
 	std::cout << "Accepted\n";
 	return newsockfd;
 }
@@ -25,12 +19,10 @@ int main(int argc, char *argv[])
 {
     std::cout << "SERVER\n";
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) { error("ERROR opening socket"); }
 	
 	// Разрешение переиспользования порта
 	const int enable = 1;
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
-	    error("setsockopt(SO_REUSEADDR) failed");
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
 	
 	// Привязка сокета к адресу
 	int portno = 21947;
@@ -39,9 +31,8 @@ int main(int argc, char *argv[])
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(portno);
-	if (bind(sockfd, (struct sockaddr *) &serv_addr,
-		 sizeof(serv_addr)) < 0) 
-		 { error("ERROR on binding"); }
+	bind(sockfd, (struct sockaddr *) &serv_addr,
+		 sizeof(serv_addr)); 
 	
 	listen(sockfd,5);
 	
@@ -57,7 +48,6 @@ int main(int argc, char *argv[])
 		std::cout << "Enter a value:\n";
 		std::cin >> buffer;
 		int n = write(newsockfd,buffer,255);
-		if (n < 0) { error("ERROR writing to socket"); }
 		
 		// Обратная связь с клиентом
 		bool lost_con = 1;
@@ -69,6 +59,5 @@ int main(int argc, char *argv[])
 	}    
 	close(newsockfd);
 	close(sockfd);
-	main(argc, argv);	
 	return 0; 
 }
